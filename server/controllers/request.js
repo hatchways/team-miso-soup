dda02f23880b7ed0f3e7a7001126790f3f241838
Jason <xyecs@users.noreply.github.com>
2021-09-28 15:56:06 -0400

    update get create requests

--- server/controllers/request.js
+++ server/controllers/request.js
@@ -5,7 +5,10 @@ const asyncHandler = require("express-async-handler");
 // @desc Gets list of all requests
 // @access Private
 exports.getRequests = asyncHandler(async (req, res, next) => {
-  const requests = Request.find();
+  const userId = req.user.id;
+
+  // Gets all requests for specified user id
+  const requests = Request.find({ user_id: userId });
 
   if (!requests) {
     res.status(404);
@@ -18,7 +21,28 @@ exports.getRequests = asyncHandler(async (req, res, next) => {
 // @route POST /request
 // @desc Create a new request
 // @access Private
-exports.createRequest = asyncHandler(async (req, res, next) => {});
+exports.createRequest = asyncHandler(async (req, res, next) => {
+  const { userId, sitterId, start, end } = req.body;
+
+  if (!userId || !sitterId || !start || !end) {
+    res.status(404);
+    throw new Error("Missing required info to make request");
+  }
+  // creates new request
+  const request = await Request.create({
+    user_id: userId,
+    sitter_id: sitterId,
+    start,
+    end,
+  });
+
+  if (request) {
+    res.status(201).json({ success: request });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
 
 // @route UPDATE /request/:id
 // @desc Update a specified request
